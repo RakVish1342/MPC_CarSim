d7baf56623fb09575fb7dd85b5603c1f665bd29d
Rxth <rakshith.vishwanatha@gmail.com>
2020-11-24 22:37:58 -0700

    Understood dimension requirements by MPC block and fixed it. Ideally, plant o/p needs to track ref signal, thus ref signal dims should equal plant output for 1-1 correspondence. Also make sure correct MV and MO signal ports/lines are selected based on need of which actual outputs of plant block need to be treated as output which should track reference.

--- reference_signal.m
+++ reference_signal.m
@@ -2,23 +2,30 @@ function output = reference_signal(input)
 
 t = input;
 
-% % f = 0.03;
-% % 
-% % del_f = sin(2*pi*f*t);
-% % del_r = 0.33*sin(2*pi*f*t);
-% % % del_r = 0;
-% % 
-% % output = [del_f, del_r];
-% % % output = [del_f; del_r];
-
-
+% % Earlier (open loop, no MPC block) I was using this block/function to 
+% % provide direct control input. After adding MPC, this block provides the
+% % reference value that the plant output (here y = lateral position) should
+% % track. So dimension is just 1.
+% f = 0.03;
 % 
+% del_f = sin(2*pi*f*t);
+% del_r = 0.33*sin(2*pi*f*t);
+% % del_r = 0;
+% 
+% output = [del_f, del_r];
+% % output = [del_f; del_r];
+
+
+% % Now (after adding MPC, closed loop system), only single referece signal
+% % for lateral position being provided
 % output = [  1, 1, 1, 1;
 %             2, 2, 2, 2];
-% 
 % output = -10;
-
-output = [-10, -10, -10, -10];
+% output = [-10, -10, -10, -10];
 % output = [-10, -10];
+f = 0.03;
+ref = sin(2*pi*f*t);
+
+output = ref;
 
 end
